75bc6847c863ba723e1e50f5e4215eb6c1de3bee
Matburger Ciprián Gergő <matburgerc.24d@acsjszki.hu>
2025-04-07 14:08:41 +0200

    Update script.js

--- dobozok-template/script.js
+++ dobozok-template/script.js
@@ -8,7 +8,7 @@ le róla azt.
  - element  JS reprezentácioja: document.getElementById('element-one')
  - esemény kiválasztása: onclick
  - funkcionalitás leirása: function () {...}
-/*
+*/
 
 // belső állapot: state
 
@@ -16,38 +16,89 @@ var isBlurred = false;
 
 document.getElementById('element-one').onclick = function () {document.getElementById('element-one').classList.toggle('blur')};
 isBlurred = !isBlurred;
-if(isBlurred) {console.dir(document.getElementById('element-one'));} else {document.getElementById('element-one').classList.remove('blur')};
+if(isBlurred) {console.dir(document.getElementById('element-one'));} else {document.getElementById('element-one').classList.remove('blur')}
 
-
-
-
-
+/*
 2. doboz:
 Ha az egérrel fölé megyünk változzon meg a háttérszíne pirosra, ha levesszük róla az egeret
 változzon vissza az eredeti színére.
 */
+
+// State
+var isHoveredOver = false;
+
+// action
+document.getElementById('element-two').onmouseover = function () {
+  // state change
+  isHoveredOver = ture;
+  // render
+  renderSecondBox();
+}
+
+// Action
+document.getElementById('element-two').onmouseout = function () {
+  // state change
+  isHoveredOver = false;
+  // render
+  renderSecondBox();
+}
+
+function renderSecondBox() {
+  if (isHoveredOver) {
+    document.getElementById('element-two').style.backgroundColor = 'red';
+  } else {
+    document.getElementById('element-two').style.backgroundColor = '';
+  }
+}
+
 
 /*
 3. doboz:
 Dupla kattintással sorsoljon egy számot 1 és 20 között és módosítsa a kapott számmal a doboz tartalmát. 
 */
 
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+ 
+document.getElementById('element-three').ondblclick = function () {
+  document.getElementById('element-three').firstElementChild.innerHTML = getRandomNumber(1, 20);
+}
 
 /*
 4. doboz:
 Kattintásra tűnjön el, majd egy 1 másodperces várakozás után ismét jelenjen meg.
 */
 
+document.getElementById('element-four').onclick = function () {
+  document.getElementById('element-four').classList.add('hidden');
+  setTimeout(function() {
+    document.getElementById('element-four').classList.remove('hidden');
+  }, 1000);
+}
+
 /*
 5. doboz:
 Kattintásra alakítsa kör alakúra az összes dobozt.
 */
 
+document.getElementById('element-five').onclick = function () {
+  var boxes = document.querySelectorAll('.shape');
+
+  for(var box of boxes){
+    box.style.borderRadius = '50%';
+  }
+}
 
 /*
 6. doboz:
 Form submit eseményre módosítsuk a doboz tartalmát az input mezőbe írt értékkel
 */
+
+
+
+
+
 
 
 /*
